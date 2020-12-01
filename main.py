cec6f5895808f963adb12d03145ab44d3c5c8a81
Smertin Nikita <smertin-nikita@mail.ru>
2020-12-01 22:46:11 +0500

    Function get questions

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import requests
 from pprint import pprint
+from datetime import datetime, timedelta
 
-if __name__ == '__main__':
+
+def get_questions(date):
+    """Returns all questions with tag 'python' since date"""
+
     response = requests.get(
         url='https://api.stackexchange.com/2.2/questions',
         params={
-            'fromdate': '2020-11-28',
+            'fromdate': date,
             'order': 'desc',
             'tagged': 'python',
             'filter': 'default',
             'site': 'stackoverflow'
         })
     response.raise_for_status()
-    pprint(response.json())
+    return response.json()
+
+
+if __name__ == '__main__':
+    delta = datetime.today() - timedelta(2)
+    fromdate = delta.strftime('%Y-%m-%d')
+
+    pprint(get_questions(fromdate))
